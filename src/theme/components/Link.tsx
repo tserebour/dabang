656f7764bf290bfcef01b9a6ac3b5abaf4137108
thegoatmufasa <tserebour1@gmail.com>
2024-10-04 11:26:10 +0000

    committ more

--- src/theme/components/Link.tsx
+++ src/theme/components/Link.tsx
@@ -1,12 +1,12 @@
 import { Theme } from '@mui/material';
 import { Components } from '@mui/material/styles/components';
-import { forwardRef } from 'react';
+// import { forwardRef } from 'react';
 // import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom';
 
 // eslint-disable-next-line
   // const LinkBehavior = forwardRef<any, Omit<RouterLinkProps, 'to'> & { href?: string }>(
-  //   (props, ref) => <RouterLink ref={ref} to={props.href || ''} {...props} />,
-  //   );
+//   (props, ref) => <RouterLink ref={ref} to={props.href || ''} {...props} />,
+//   );
 
 const Link: Components<Omit<Theme, 'components'>>['MuiLink'] = {
   defaultProps: {
