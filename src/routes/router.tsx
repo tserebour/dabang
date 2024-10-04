c3a29e0adb1d6f4fdcc0f39deaf3f3bc99b84628
thegoatmufasa <tserebour1@gmail.com>
2024-10-04 04:17:13 +0000

    first commit

---
+++ src/routes/router.tsx
@@ -0,0 +1,101 @@
+import { Suspense, lazy } from 'react';
+import { Outlet, createBrowserRouter } from 'react-router-dom';
+import paths, { rootPaths } from './paths';
+
+const App = lazy(() => import('App'));
+const MainLayout = lazy(() => import('layouts/main-layout'));
+const AuthLayout = lazy(() => import('layouts/auth-layout'));
+const Dashboard = lazy(() => import('pages/dashboard/Dashboard'));
+const SignIn = lazy(() => import('pages/authentication/SignIn'));
+const SignUp = lazy(() => import('pages/authentication/SignUp'));
+const Page404 = lazy(() => import('pages/errors/Page404'));
+
+import PageLoader from 'components/loading/PageLoader';
+import Progress from 'components/loading/Progress';
+import Leaderboard from 'pages/leaderboard/Leaderboard';
+import { Feedback } from '@mui/icons-material';
+import UserFeedback from 'pages/userfeedback/UserFeedback';
+import ProjectDashboard from 'pages/projectdashboard/ProjectDashboard';
+
+export const routes = [
+  {
+    element: (
+      <Suspense fallback={<Progress />}>
+        <App />
+      </Suspense>
+    ),
+    children: [
+      {
+        path: rootPaths.root,
+        element: (
+          <MainLayout>
+            <Suspense fallback={<PageLoader />}>
+              <Outlet />
+            </Suspense>
+          </MainLayout>
+        ),
+        children: [
+          {
+            index: true,
+            element: <Dashboard />,
+          },
+        ],
+      },
+      {
+        path: rootPaths.authRoot,
+        element: <AuthLayout />,
+        children: [
+          {
+            path: paths.signin,
+            element: <SignIn />,
+          },
+          {
+            path: paths.signup,
+            element: <SignUp />,
+          },
+        ],
+      },
+
+      {
+        path: paths.leaderboard,
+        element: (
+          <MainLayout>
+            <Suspense fallback={<PageLoader />}>
+              <Leaderboard />
+            </Suspense>
+          </MainLayout>
+        ),
+      },
+
+      {
+        path: paths.projectdashboard,
+        element: (
+          <MainLayout>
+            <Suspense fallback={<PageLoader />}>
+              <ProjectDashboard />
+            </Suspense>
+          </MainLayout>
+        ),
+      },
+
+      {
+        path: paths.feedback,
+        element: (
+          <MainLayout>
+            <Suspense fallback={<PageLoader />}>
+              <UserFeedback />
+            </Suspense>
+          </MainLayout>
+        ),
+      },
+      {
+        path: '*',
+        element: <Page404 />,
+      },
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes, { basename: '/dabang' });
+
+export default router;
