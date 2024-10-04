61e723b8bb8435052d79e54976de181545e347c0
tserebour <tserebour@users.noreply.github.com>
2024-10-04 12:11:10 +0000

    deploy: d2694292f3ba01c3204587bc283d4cc0aaea78d8

---
+++ assets/App-Bz_PA5U5.js
@@ -0,0 +1,1 @@
+import{j as t,O as s}from"./index-C3svrTrj.js";const e=()=>t.jsx(s,{});export{e as default};
--- assets/App-zerUyjvf.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as s}from"./index-BH0Fy5sJ.js";const e=()=>t.jsx(s,{});export{e as default};
