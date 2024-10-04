1cdf40af0264ceb2b0322449230738c3ffb09472
tserebour <tserebour@users.noreply.github.com>
2024-10-04 11:27:19 +0000

    deploy: 656f7764bf290bfcef01b9a6ac3b5abaf4137108

---
+++ assets/App-zerUyjvf.js
@@ -0,0 +1,1 @@
+import{j as t,O as s}from"./index-BH0Fy5sJ.js";const e=()=>t.jsx(s,{});export{e as default};
---
+++ assets/index-BToc1T73.js
@@ -0,0 +1,1 @@
+import{j as t,N as e,O as r}from"./index-BH0Fy5sJ.js";const s=()=>t.jsx(e,{justifyContent:"center",alignItems:"center",minHeight:"100vh",bgcolor:"neutral.lighter",children:t.jsx(r,{})});export{s as default};
