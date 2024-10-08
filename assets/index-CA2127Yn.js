620b74ae2f29949dc484d54a109ec7f4b83a3dd7
tserebour <tserebour@users.noreply.github.com>
2024-10-08 16:50:45 +0000

    deploy: 6a69f3bf9d57f863ccf5e11675221fd8cf7439f8

--- assets/index-CA2127Yn.js
+++ assets/index-CA2127Yn.js
@@ -1,4 +1,4 @@
-import{M as U_}from"./index-B5X692Aa.js";/*! *****************************************************************************
+import{M as U_}from"./index-BgqoB4Ix.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
--- assets/index-mMbvh8Q5.js
+++
@@ -1,1 +0,0 @@
-import{j as t,S as e,O as r}from"./index-B5X692Aa.js";const s=()=>t.jsx(e,{justifyContent:"center",alignItems:"center",minHeight:"100vh",bgcolor:"neutral.lighter",children:t.jsx(r,{})});export{s as default};
