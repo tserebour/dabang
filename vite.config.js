6a69f3bf9d57f863ccf5e11675221fd8cf7439f8
thegoatmufasa <tserebour1@gmail.com>
2024-10-08 16:49:39 +0000

    new2

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,22 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    react(),
-    checker({
-      typescript: true,
-      eslint: {
-        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
-      },
-    }),
-  ],
-  preview: {
-    port: 5000,
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
-  base: '/',
+    plugins: [
+        tsconfigPaths(),
+        react(),
+        checker({
+            typescript: true,
+            eslint: {
+                lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+            },
+        }),
+    ],
+    preview: {
+        port: 5000,
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+    },
+    base: '/dabang/',
 });
