fe26f09886a0daee0497f40283427f98c485378f
tserebour <tserebour@users.noreply.github.com>
2024-10-09 10:18:20 +0000

    deploy: 6423c8a73daf8630972b025fa0dd94f6da90af92

---
+++ assets/index-mMbvh8Q5.js
@@ -0,0 +1,1 @@
+import{j as t,S as e,O as r}from"./index-B5X692Aa.js";const s=()=>t.jsx(e,{justifyContent:"center",alignItems:"center",minHeight:"100vh",bgcolor:"neutral.lighter",children:t.jsx(r,{})});export{s as default};
